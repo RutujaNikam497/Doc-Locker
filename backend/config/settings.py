ef0d145a0f21bf288b7af9612646a7697ed525f8
RutujaNikam497 <rutujanikam2005@gmail.com>
2026-07-22 15:31:19 +0530

    Refactor dashboard module

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "apps.users",
     "apps.documents",
     "apps.dashboard",
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -141,6 +142,11 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
+    ),
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ),
 }
 
